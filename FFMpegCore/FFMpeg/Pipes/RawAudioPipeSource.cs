a279f4ba53dcb272820acc24262243b399da3856
Frooxius <tomas.mariancik@gmail.com>
2026-06-05 20:14:49 +0200

    feat: Added RawFloatAudioPipeSource

--- FFMpegCore/FFMpeg/Pipes/RawAudioPipeSource.cs
+++ FFMpegCore/FFMpeg/Pipes/RawAudioPipeSource.cs
@@ -2,7 +2,7 @@
 
 /// <summary>
 ///     Implementation of <see cref="IPipeSource" /> for a raw audio stream that is gathered from <see cref="IEnumerator{IAudioFrame}" />.
-///     It is the user's responbility to make sure the enumerated samples match the configuration provided to this pipe.
+///     It is the user's responsibility to make sure the enumerated samples match the configuration provided to this pipe.
 /// </summary>
 public class RawAudioPipeSource : IPipeSource
 {
